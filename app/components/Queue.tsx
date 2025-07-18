9ec156ce1d30797324baf6b419765b2ab97d6b89
trueshizus <maj.herrera@pm.me>
2025-07-18 22:18:01 +0200

    Add Queue component for displaying selectable item lists

---
+++ app/components/Queue.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import RadioLink from "./RadioLink";
+
+type Props = {
+  name: string;
+  items: Array<any>;
+  component: (props: any) => React.JSX.Element;
+};
+
+export default function Queue({ name, items, component }: Props) {
+  return (
+    <fieldset className="border border-solid bg-zinc-950/5 border-zinc-950 has-[:focus]:border-4 p-2 h-full overflow-hidden">
+      <legend className="bg-zinc-950 min-w-full text-center text-white mx-auto">
+        {name}
+      </legend>
+
+      <section className="border border-solid bg-zinc-950/5 border-zinc-950 has-[:focus]:border-4 overflow-y-scroll h-full scrollbar">
+        {items.map((item: any) => (
+          <div key={item._id}>
+            <label
+              htmlFor={item._id}
+              className="border border-solid border-zinc-950 has-[:checked]:bg-zinc-950/5 has-[:checked]:border-l-8 p-2 flex flex-col"
+            >
+              <RadioLink id={item._id} name={name} />
+              <div>{component({ ...item })}</div>
+            </label>
+          </div>
+        ))}
+      </section>
+    </fieldset>
+  );
+}
